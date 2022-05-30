4363257cb16d0889bea2241dfab42cddfee6ea29
Joe Harrison <joe@j-harrison.co.uk>
2022-05-30 15:23:40 +0100

    Final Commit

--- startup.sh
+++ startup.sh
@@ -13,10 +13,10 @@ sudo echo "USERNAMEHERE ALL=(ALL) NOPASSWD:/bin/mkdir,/bin/mv,/bin/cat,/bin/rm,/
 
 # Install Docker
 sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release -y
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --batch --yes
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce
+sudo apt update -y
+sudo apt install docker-ce nano -y
 
 # Install Docker Compose - manually add your username to the path
 sudo mkdir -p /home/USERNAMEHERE/.docker/cli-plugins
